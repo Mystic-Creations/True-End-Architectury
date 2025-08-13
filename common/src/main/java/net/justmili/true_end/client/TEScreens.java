dc4c3c5edc2d6586f0e1defd2487d94cae1cb708
eetgeenappels <eetgeenappels@gmail.com>
2025-08-13 11:11:04 +0200

    properly implement screen registration on the server

--- common/src/main/java/net/justmili/true_end/client/TEScreens.java
+++ common/src/main/java/net/justmili/true_end/client/TEScreens.java
@@ -1,20 +1,25 @@
 package net.justmili.true_end.client;
 
+import dev.architectury.registry.menu.MenuRegistry;
 import dev.architectury.registry.registries.DeferredRegister;
 import dev.architectury.registry.registries.DeferredSupplier;
+import dev.architectury.registry.registries.RegistrySupplier;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.justmili.true_end.TrueEndCommon;
 import net.justmili.true_end.client.screens.BlackOverlay;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.MenuConstructor;
 import net.minecraft.world.inventory.MenuType;
+import net.minecraft.world.item.ItemStack;
 
-public class Guis {
+public class TEScreens {
     public static DeferredRegister<MenuType<?>> REGISTRY = DeferredRegister.create(TrueEndCommon.MOD_ID, Registries.MENU);
-
-    public static final DeferredSupplier<MenuType<BlackOverlay>> BLACK_SCREEN =
-            REGISTRY.register("black_screen", new MenuType<>((MenuConstructor<BlackOverlay>) BlackOverlay::new));
-
+    public static final RegistrySupplier<MenuType<BlackOverlay>> BLACK_SCREEN = REGISTRY.register("black_screen", ()->MenuRegistry.ofExtended(BlackOverlay::new));
     public static void register() {
         REGISTRY.register();
     }
--- common/src/main/java/net/justmili/true_end/client/TrueEndCommonClient.java
+++ common/src/main/java/net/justmili/true_end/client/TrueEndCommonClient.java
@@ -1,10 +1,13 @@
 package net.justmili.true_end.client;
 
 import dev.architectury.registry.client.level.entity.EntityRendererRegistry;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.justmili.true_end.client.init.TrueEndParticles;
 import net.justmili.true_end.client.renderer.UnknownEntityRenderer;
 import net.justmili.true_end.init.TEEntities;
 
+@Environment(EnvType.CLIENT)
 public final class TrueEndCommonClient {
 
     public static void init() {
