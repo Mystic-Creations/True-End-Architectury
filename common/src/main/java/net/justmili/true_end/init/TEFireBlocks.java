56c9659f42a72180204f7887bac733e3d24cb9b6
eetgeenappels <eetgeenappels@gmail.com>
2025-08-18 16:47:31 +0200

    fix funny refactoring

--- common/src/main/java/net/justmili/true_end/init/TEFireBlocks.java
+++ common/src/main/java/net/justmili/true_end/init/TEFireBlocks.java
@@ -6,17 +6,17 @@ import net.justmili.true_end.registry.ArsonRegistryReference;
 public class TEFireBlocks {
     public static ArsonRegistry REGISTRY = ArsonRegistry.createInstance();
 
-    public static ArsonRegistryReference WOOD = REGISTRY.registerBlockEntry(TEBlocks.WOOD.get(), new ArsonRegistry.Arsonabilty(20, 40));
-    public static ArsonRegistryReference WOOD_SIX_SIDED = REGISTRY.registerBlockEntry(TEBlocks.WOOD_6_SIDED.get(), new ArsonRegistry.Arsonabilty(20, 40));
-    public static ArsonRegistryReference FENCE = REGISTRY.registerBlockEntry(TEBlocks.FENCE.get(), new ArsonRegistry.Arsonabilty(20, 40));
-    public static ArsonRegistryReference FENCE_GATE = REGISTRY.registerBlockEntry(TEBlocks.FENCE_GATE.get(), new ArsonRegistry.Arsonabilty(20, 40));
-    public static ArsonRegistryReference DOOR = REGISTRY.registerBlockEntry(TEBlocks.DOOR.get(), new ArsonRegistry.Arsonabilty(20, 40));
-    public static ArsonRegistryReference STAIRS = REGISTRY.registerBlockEntry(TEBlocks.WOODEN_STAIRS.get(), new ArsonRegistry.Arsonabilty(20, 40));
-    public static ArsonRegistryReference TRAPDOOR = REGISTRY.registerBlockEntry(TEBlocks.TRAPDOOR.get(), new ArsonRegistry.Arsonabilty(20, 40));
-    public static ArsonRegistryReference PRESSURE_PLATE = REGISTRY.registerBlockEntry(TEBlocks.PRESSURE_PLATE.get(), new ArsonRegistry.Arsonabilty(20, 40));
-    public static ArsonRegistryReference SAPLING = REGISTRY.registerBlockEntry(TEBlocks.SAPLING.get(), new ArsonRegistry.Arsonabilty(20, 40));
-    public static ArsonRegistryReference BUTTON = REGISTRY.registerBlockEntry(TEBlocks.BUTTON.get(), new ArsonRegistry.Arsonabilty(20, 40));
-    public static ArsonRegistryReference LEAVES = REGISTRY.registerBlockEntry(TEBlocks.LEAVES.get(), new ArsonRegistry.Arsonabilty(20, 40));
+    public static ArsonRegistry.ArsonRegistryReference WOOD = REGISTRY.registerBlockEntry(TEBlocks.WOOD.get(), new ArsonRegistry.Arsonability(20, 40));
+    public static ArsonRegistry.ArsonRegistryReference WOOD_SIX_SIDED = REGISTRY.registerBlockEntry(TEBlocks.WOOD_6_SIDED.get(), new ArsonRegistry.Arsonability(20, 40));
+    public static ArsonRegistry.ArsonRegistryReference FENCE = REGISTRY.registerBlockEntry(TEBlocks.FENCE.get(), new ArsonRegistry.Arsonability(20, 40));
+    public static ArsonRegistry.ArsonRegistryReference FENCE_GATE = REGISTRY.registerBlockEntry(TEBlocks.FENCE_GATE.get(), new ArsonRegistry.Arsonability(20, 40));
+    public static ArsonRegistry.ArsonRegistryReference DOOR = REGISTRY.registerBlockEntry(TEBlocks.DOOR.get(), new ArsonRegistry.Arsonability(20, 40));
+    public static ArsonRegistry.ArsonRegistryReference STAIRS = REGISTRY.registerBlockEntry(TEBlocks.WOODEN_STAIRS.get(), new ArsonRegistry.Arsonability(20, 40));
+    public static ArsonRegistry.ArsonRegistryReference TRAPDOOR = REGISTRY.registerBlockEntry(TEBlocks.TRAPDOOR.get(), new ArsonRegistry.Arsonability(20, 40));
+    public static ArsonRegistry.ArsonRegistryReference PRESSURE_PLATE = REGISTRY.registerBlockEntry(TEBlocks.PRESSURE_PLATE.get(), new ArsonRegistry.Arsonability(20, 40));
+    public static ArsonRegistry.ArsonRegistryReference SAPLING = REGISTRY.registerBlockEntry(TEBlocks.SAPLING.get(), new ArsonRegistry.Arsonability(20, 40));
+    public static ArsonRegistry.ArsonRegistryReference BUTTON = REGISTRY.registerBlockEntry(TEBlocks.BUTTON.get(), new ArsonRegistry.Arsonability(20, 40));
+    public static ArsonRegistry.ArsonRegistryReference LEAVES = REGISTRY.registerBlockEntry(TEBlocks.LEAVES.get(), new ArsonRegistry.Arsonability(20, 40));
 
     public static void register() {
         ArsonRegistry.register(REGISTRY);
